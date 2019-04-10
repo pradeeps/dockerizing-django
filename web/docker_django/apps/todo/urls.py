45d085599d7a8d7795c42bb38831714c30df40b0
Pradeep Susarla <pradeep@pradeeps.org>
2019-04-10 22:12:26 +0100

    added pipenv

--- web/docker_django/apps/todo/urls.py
+++ web/docker_django/apps/todo/urls.py
@@ -1,7 +1,6 @@
-from django.urls import re_path
-
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.home, name='home'),
+    path('', views.home, name='home'),
 ]
--- web/docker_django/apps/todo/views.py
+++ web/docker_django/apps/todo/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import Item
 from redis import Redis
-
+from django.http import HttpResponse
 
 redis = Redis(host='redis', port=6379)
 
-
 def home(request):
+    return HttpResponse("Hello world from Docker")
+
+def home1(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
